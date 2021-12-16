d3f78c9714a9cca0f56525a1da7d715e33239ea4
Joseph Martin <josephmartin1350@gmail.com>
2021-12-16 11:41:19 -0700

    select DOM elements and added comments

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ const allDirectorsURL = 'https://api.catalogopolis.xyz/v1/directors'
     // helpers
 const create = el => document.createElement(el)
 const select = el => document.querySelector(el)
+
+    // grab stuff
+const categoryContainer = select('#category-container')
+const subcategoryContainer = select('#subcategory-container')
+const episodeDetails = select('#episode-detail-container')
+// might rename
+const likesAndCommentsContainer = select('#likes-comments-container')
+//might rename
+const seasonsButton = select('#seasons-button')
+const doctorsButton = select('#doctors-button')
+const companionButton = select('#companions-button')
+let directorButton = select('#director-button')
