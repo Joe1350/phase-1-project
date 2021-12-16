7491b812defd89143faa3dcf725d3d3c00a30e74
Joseph Martin <josephmartin1350@gmail.com>
2021-12-16 11:35:05 -0700

    urls saved to variables

---
+++ src/index.js
@@ -0,0 +1,5 @@
+    // URL's
+const allSeasonsURL = 'https://api.catalogopolis.xyz/v1/seasons/'
+const allDoctorsURL = 'https://api.catalogopolis.xyz/v1/doctors'
+const allCompanionsURL = 'https://api.catalogopolis.xyz/v1/companions'
+const allDirectorsURL = 'https://api.catalogopolis.xyz/v1/directors'
