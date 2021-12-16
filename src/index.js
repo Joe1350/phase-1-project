2dcb0d930a493e7d1e8a8e4c68171de9993a0dde
Joseph Martin <josephmartin1350@gmail.com>
2021-12-16 11:36:21 -0700

    added helper functions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const allSeasonsURL = 'https://api.catalogopolis.xyz/v1/seasons/'
 const allDoctorsURL = 'https://api.catalogopolis.xyz/v1/doctors'
 const allCompanionsURL = 'https://api.catalogopolis.xyz/v1/companions'
 const allDirectorsURL = 'https://api.catalogopolis.xyz/v1/directors'
+
+    // helpers
+const create = el => document.createElement(el)
+const select = el => document.querySelector(el)
